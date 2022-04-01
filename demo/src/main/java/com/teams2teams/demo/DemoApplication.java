a3d809b84ddf6d1689308b5c2e3ed715ad6578f8
Matusek-Faludi <matusek.faludi.laszlo@gmail.com>
2022-04-01 20:04:22 +0200

    első commit

---
+++ demo/src/main/java/com/teams2teams/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.teams2teams.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Teams2teams app started");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Teams2teams app finished");
+	}
+
+}
