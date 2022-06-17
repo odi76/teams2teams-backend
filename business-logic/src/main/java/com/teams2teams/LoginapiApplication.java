99900d8bcf2a19378cd056caa4d8577622691f39
Matusek-Faludi <matusek.faludi.laszlo@gmail.com>
2022-06-17 08:59:45 +0200

    logolás beépítve

--- business-logic/src/main/java/com/teams2teams/LoginapiApplication.java
+++ business-logic/src/main/java/com/teams2teams/LoginapiApplication.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.teams2teams.model.User;
 import com.teams2teams.repositories.UserRepository;
 import com.teams2teams.service.UserService;
@@ -18,7 +21,14 @@ import com.teams2teams.controller.*;
 @SpringBootApplication
 public class LoginapiApplication implements CommandLineRunner{
 
+	private static final Logger logger = LoggerFactory.getLogger(LoginapiApplication.class);
+
+
 	public static void main(String... args) {
+
+		// demo log kiírás:
+		logger.info("this is a T2T info message");
+
 		SpringApplication.run(LoginapiApplication.class, args);
 	}
 
@@ -28,13 +38,17 @@ public class LoginapiApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) {
 
+		// a system kiírás törölhető
 		System.out.println("új userek felvétele");
+
+		logger.info("Create default users");
 		User admin = new User( "admin2@a.hu", "titkos");
 		User istvan = new User("istvan2@i.hu", "iii");
 		User attila = new User("attila2@a.hu", "aaa");
 		User laci = new User("laci@l.hu", "lll");
 
 		userRepository.saveAllAndFlush(List.of(admin, istvan, attila, laci));
+		logger.info("Default users saved into database");
 
 	}
 
