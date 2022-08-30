e8df3d5f32460e48c5d449c6193be5b5ccf2119b
Matusek-Faludi <matusek.faludi.laszlo@gmail.com>
2022-08-30 20:51:40 +0200

    CreateDemoUser külön API

--- src/main/java/com/teams2teams/backend/Teams2TeamsBackendApplication.java
+++ src/main/java/com/teams2teams/backend/Teams2TeamsBackendApplication.java
@@ -1,8 +1,5 @@
 package com.teams2teams.backend;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,37 +7,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.teams2teams.backend.loginapi.User;
-import com.teams2teams.backend.loginapi.UserRepository;
-
 
 @SpringBootApplication
-public class Teams2TeamsBackendApplication implements CommandLineRunner{
 
+public class Teams2TeamsBackendApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(Teams2TeamsBackendApplication.class, args);
 	}
-
-	@Autowired
-	private UserRepository userRepository;
 
 	@Override
 	public void run(String... args) {
 
 		// a system kiírás törölhető
-		System.out.println("Új userek létrehozása");
+		//System.out.println("Új userek létrehozása");
 
 		
 
 		
-		User admin = new User( "admin2@a.hu", "titkos222");
+		//User admin = new User( "admin2@a.hu", "titkos222");
 		//egy kiírása: userRepository.save(admin);
-		User istvan = new User("istvan2@i.hu", "iii");
-		User attila = new User("attila2@a.hu", "aaa");
-		User laci = new User("laci@l.hu", "lll");
+		//User istvan = new User("istvan2@i.hu", "iii");
+		//User attila = new User("attila2@a.hu", "aaa");
+		//User laci = new User("laci@l.hu", "lll");
 
 		//Több kiírása
-		userRepository.saveAll(List.of(admin, istvan, attila, laci));
+		//userRepository.saveAll(List.of(admin, istvan, attila, laci));
 
 	}
 
---
+++ src/main/java/com/teams2teams/backend/demofiles/CreateDemoUser.java
@@ -0,0 +1,49 @@
+package com.teams2teams.backend.demofiles;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teams2teams.backend.loginapi.RegistrateApiDelegateImpl;
+import com.teams2teams.backend.openapi.model.RegistrationData;
+
+
+
+@RestController
+@RequestMapping("/createdemouser")
+public class CreateDemoUser {
+     
+    
+
+    @GetMapping()
+    public void Demousers()
+    {
+        //@Autowired
+       
+        System.out.println("Demo userek létrehozása");
+        
+        RegistrateApiDelegateImpl registrateApiDelegateImpl; 
+        registrateApiDelegateImpl = new RegistrateApiDelegateImpl();
+        
+        RegistrationData u1;
+        u1 = new RegistrationData();
+        u1.setEmail("admin@email.hu");
+        u1.setPassword("1111");
+        registrateApiDelegateImpl.registrate(u1);
+
+        System.out.println("U1 user létrehozva");
+
+        RegistrationData u2;
+        u2 = new RegistrationData();
+        u2.setEmail("istvan@email.hu");
+        u2.setPassword("1111");
+        registrateApiDelegateImpl.registrate(u2);
+
+        System.out.println("U2 user létrehozva");
+
+
+    
+    }
+}
+    
+
