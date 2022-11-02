183d31ef3e9245b161ffac2ad87710b386a7a740
Dányi Animut <danyianimut@gmail.com>
2022-11-02 20:49:41 +0100

    CORS: DELETE method was allowed too

--- src/main/java/com/teams2teams/backend/Teams2TeamsBackendApplication.java
+++ src/main/java/com/teams2teams/backend/Teams2TeamsBackendApplication.java
@@ -20,7 +20,8 @@ public class Teams2TeamsBackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "DELETE");
 			}
 		};
 	}
